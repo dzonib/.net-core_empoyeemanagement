8e0045e2187c7598e677977884bd2e3c5c096bd1
bosnjaknikola <nikola.bosnjak@wayseven.com>
2019-09-19 16:47:41 +0200

    did stuff

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public JsonResult Index()
+        {
+            var employee = _employeeRepository.GetEmployee(1).Name;
+
+            return Json(employee);
+        }
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -22,6 +23,8 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +35,10 @@ namespace EmployeeManagement
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseFileServer();
+            app.UseStaticFiles();
+
+            //search for Index method in HomeController and fires it
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
